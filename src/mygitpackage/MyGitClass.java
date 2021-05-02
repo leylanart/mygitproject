6f8e0dcf505692974867736c8505ad5c1fd5616d
leylanart <77584438+leylanart@users.noreply.github.com>
2021-05-02 07:08:18 -0500

    Update MyGitClass.java
    changing the git hub codes

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("My second commit");
 		System.out.println("My third commit");
+		System.out.println("remote codes");
 	}
 
 }
